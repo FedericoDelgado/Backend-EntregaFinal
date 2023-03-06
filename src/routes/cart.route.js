cfdfaf8d3cdf2dc783af26ecb78ca9c41d474cfc
Federico Delgado <delgadofefe@gmail.com>
2023-03-06 15:25:44 -0300

    Entrega del proyecto final Backend

---
+++ src/routes/cart.route.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {
+  getAllProductsByIdCart,
+  createCart,
+  viewCart,
+  addProductToCart,
+  deleteCartById,
+  deleteProductById,
+} from '../controllers/cart.controller.js';
+
+const router = express.Router(); 
+
+router.get('/:id/product', getAllProductsByIdCart);
+router.get('/', viewCart);
+router.post('/', createCart);
+router.post('/add-product', addProductToCart);
+router.post('/delete-product', deleteProductById);
+
+
+router.delete('/:id', deleteCartById);
+
+export default router;
